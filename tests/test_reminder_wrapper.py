8057ee35dbb18d5367e0c2d61e22c2e55b67e4dc
jannis <jannisko99@gmail.com>
2019-10-15 10:32:18 +0200

    add initial tests

---
+++ tests/test_reminder_wrapper.py
@@ -0,0 +1,22 @@
+import os, dotenv
+
+# if reminder api credentials haven't been set before starting the script,
+# try to get them from .env file
+if(os.environ.get('SID') == None):
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    env_path = os.path.join(file_path, '../auth.env')
+    dotenv.load_dotenv(dotenv_path=env_path)
+
+from google_reminder_api_wrapper.reminder_api import ReminderApi
+
+def test_list():
+    api = ReminderApi()
+    reminders = api.list()
+    assert type(reminders) == dict
+
+def test_create():
+    client_assigned_id = 'test_reminder_id'
+    api = ReminderApi()
+    api.create('test_reminder', taskId={'clientAssignedId': client_assigned_id})
+    reminder = api.get('', taskId={'clientAssignedId': client_assigned_id})
+    assert reminder['task'][0]['taskId']['clientAssignedId'] == client_assigned_id
